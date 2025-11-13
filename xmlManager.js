23c51f2aade6c5ae7f58b6f4a6766ca4ef25bad3
Fahim <49230752+evilgodfahim@users.noreply.github.com>
2025-11-13 20:11:54 +0600

    Update xmlManager.js

--- xmlManager.js
+++ xmlManager.js
@@ -5,30 +5,42 @@ const XML_FILE = './articles.xml';
 const MAX_ITEMS = 500;
 
 export async function loadXML() {
-    if (!fs.existsSync(XML_FILE)) return { rss: { $: { version: '2.0' }, channel: { title: "Kalbela News", link: "https://www.kalbela.com/ajkerpatrika", description: "Latest news from Kalbela.com", item: [] } } };
+    if (!fs.existsSync(XML_FILE)) {
+        return {
+            rss: {
+                $: { version: '2.0' },
+                channel: {
+                    title: "Kalbela News",
+                    link: "https://www.kalbela.com/ajkerpatrika",
+                    description: "Latest news from Kalbela.com",
+                    item: []
+                }
+            }
+        };
+    }
     const content = await fs.readFile(XML_FILE, 'utf8');
     const data = await parseStringPromise(content);
     data.rss.channel.item = data.rss.channel.item || [];
     return data;
 }
 
-export async function saveArticle(article) {
-    const data = await loadXML();
-
-    // Create RSS <item>
-    const rssItem = {
-        title: article.title,
-        link: article.link,
-        description: article.image ? `<img src="${article.image}" />` : '',
-        pubDate: new Date(article.date).toUTCString()
-    };
-
-    // Add newest first
-    data.rss.channel.item.unshift(rssItem);
-
-    // Limit to MAX_ITEMS
-    if (data.rss.channel.item.length > MAX_ITEMS) {
-        data.rss.channel.item = data.rss.channel.item.slice(0, MAX_ITEMS);
+// Modified saveArticle to allow forcing a full XML save
+export async function saveArticle({ title, link, image, date, forceData } = {}) {
+    let data;
+    if (forceData) {
+        data = forceData;
+    } else {
+        data = await loadXML();
+        const rssItem = {
+            title,
+            link,
+            description: image ? `<img src="${image}" />` : '',
+            pubDate: new Date(date).toUTCString()
+        };
+        data.rss.channel.item.unshift(rssItem);
+        if (data.rss.channel.item.length > MAX_ITEMS) {
+            data.rss.channel.item = data.rss.channel.item.slice(0, MAX_ITEMS);
+        }
     }
 
     const builder = new Builder({ xmldec: { version: '1.0', encoding: 'UTF-8' } });
